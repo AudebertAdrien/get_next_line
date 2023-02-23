2d8106eb86607192c6053aabfa905d6803e13b74
Adrien Audebert <aaudeber@student.42perpignan.fr>
2023-02-23 17:13:38 +0100

    get_next_line mandatory done

--- get_next_line.c
+++ get_next_line.c
@@ -6,48 +6,11 @@
 /*   By: aaudeber <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/19 16:56:24 by aaudeber          #+#    #+#             */
-/*   Updated: 2023/02/23 15:27:17 by aaudeber         ###   ########.fr       */
+/*   Updated: 2023/02/23 17:12:53 by aaudeber         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
-
-char	*ft_read(char *stash, int fd)
-{
-	char		buf[BUFFER_SIZE + 1];
-	int			is_found;
-
-	is_found = 0;
-	if (!stash)
-			stash = "";
-	while (!is_found)
-	{
-		read(fd, buf, BUFFER_SIZE);
-		stash = ft_strjoin(stash, buf);
-		if (!stash)
-			return (NULL);
-		is_found = ft_find_byte(stash);
-	}
-	return (stash);
-}
-
-char	*ft_get_line(char *stash)
-{
-	char	*line;
-	int		i;
-	int		j;
-
-	i = ft_find_byte(stash);
-	j = 0;
-	line = (char *)malloc(sizeof(char *) * i + 1);
-	while (j < i)
-	{
-		line[j] = stash[j];
-		j++;
-	}
-
-	return (line);
-}
 
 char	*ft_clean_stash(char *stash)
 {
@@ -67,7 +30,47 @@ char	*ft_clean_stash(char *stash)
 		i++;
 	}
 	new_stash[j] = '\0';
+	if (stash)
+		free(stash);
 	return (new_stash);
+}
+
+char	*ft_get_line(char *stash)
+{
+	char	*line;
+	int		i;
+	int		j;
+	i = ft_find_byte(stash);
+
+	j = 0;
+	line = (char *)malloc(sizeof(char *) * i + 1);
+	if (!line)
+		return (NULL);
+	while (j < i)
+	{
+		line[j] = stash[j];
+		j++;
+	}
+	line[j] = '\0';
+	return (line);
+}
+
+char	*ft_read(char *stash, int fd)
+{
+	char		buf[BUFFER_SIZE + 1];
+	int			is_found;
+
+	is_found = 0;
+	if (!stash)
+			stash = "";
+	while (!is_found && read(fd, buf, BUFFER_SIZE)) 
+	{
+		stash = ft_strjoin(stash, buf);
+		if (!stash)
+			return (NULL);
+		is_found = ft_find_byte(stash);
+	}
+	return (stash);
 }
 
 char *get_next_line(int fd)
