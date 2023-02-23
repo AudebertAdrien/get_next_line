f1fb4a7ac2f2e578b9bef69c4e52d0f104e778bc
Adrien Audebert <aaudeber@student.42perpignan.fr>
2023-02-23 12:10:19 +0100

    merge

--- get_next_line.c
+++ get_next_line.c
@@ -6,33 +6,75 @@
 /*   By: aaudeber <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/19 16:56:24 by aaudeber          #+#    #+#             */
-/*   Updated: 2023/02/21 16:36:37 by aaudeber         ###   ########.fr       */
+/*   Updated: 2023/02/23 12:08:56 by aaudeber         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
+char	*ft_read(int fd)
+{
+	static char	*stash;
+	char		buf[BUFFER_SIZE + 1];
+	int			bytes_read;
+	int			is_found;
+
+	is_found = 0;
+	while (!is_found)
+	{
+		bytes_read = read(fd, buf, BUFFER_SIZE);
+		if (!stash)
+			stash = "";
+		stash = ft_strjoin(stash, buf);
+		is_found = ft_finds_byte_end_line(stash);	
+		
+		if (!stash)
+			return (NULL);
+
+		if (is_found)
+			is_found = 1;
+	}
+	return (stash);
+}
+
+char *get_next_line(int fd)
+{
+	return(ft_read(fd));
+}
+/*
 char *get_next_line(int fd)
 {
 	int		bytes_read;
 	int		is_found;
+	char	buf[BUFFER_SIZE + 1];
 	char	*ptr;
-	static char	buf[BUFFER_SIZE];
 
+	static char	*save;
 	is_found = 0;
 	while (!is_found || bytes_read == BUFFER_SIZE)
 	{
 		bytes_read = read(fd, buf, BUFFER_SIZE);
-		ptr = ft_strjoin(ptr, buf);	
+		is_found = ft_finds_byte_end_line(buf);	
+
+		if (save)
+		{
+			ptr = ft_strjoin(save, buf);
+
+		}
+		
+		if (is_found)
+		{
+			save = ft_substr(buf, is_found, bytes_read);
+			ptr = ft_strjoin(ptr, ft_substr(buf, 0, is_found));
+			return (ptr);
+		}	
+
+		ptr = ft_strjoin(ptr, buf);
 		if (!ptr)
 			return (NULL);
-		
-		if (ft_find_end_line(buf))
-		{
-			is_found = 1;
-		}
 	}
 
 	printf("\n");
 	return (ptr);
 }
+*/
--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -6,7 +6,7 @@
 /*   By: aaudeber <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/19 17:09:37 by aaudeber          #+#    #+#             */
-/*   Updated: 2023/02/21 16:09:58 by aaudeber         ###   ########.fr       */
+/*   Updated: 2023/02/23 10:47:28 by aaudeber         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,7 +22,37 @@ int	ft_strlen(const char *str)
 	return (i);
 }
 
-int	ft_find_end_line(char *b)
+char	*ft_substr(char const *s, unsigned int start, size_t len)
+{
+	char	*ptr;
+	size_t	i;
+	size_t	s_len;
+
+	s_len = ft_strlen(s);
+	i = 0;
+	if (!s)
+		return (NULL);
+	if (start >= s_len)
+	{
+		ptr = (malloc(1));
+		*ptr = '\0';
+		return (ptr);
+	}
+	if (len > s_len - start)
+		len = s_len - start;
+	ptr = (char *)malloc(sizeof(char) * (len + 1));
+	if (!ptr)
+		return (NULL);
+	while (i < len)
+	{
+		ptr[i] = s[start + i];
+		i++;
+	}
+	ptr[i] = '\0';
+	return (ptr);
+}
+
+int	ft_finds_byte_end_line(char *b)
 {
 	int	i;
 
